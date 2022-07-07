537166c4048dccdf350c184165528177ec6535d3
wmxpy <wm@mengw.io>
2022-07-06 23:21:27 -0500

    add proxy

--- src/axios-proxy.ts
+++ src/axios-proxy.ts
@@ -4,31 +4,33 @@
  * @description Axios Proxy
  */
 
-import { SudoRPCCall, SudoRPCCallProxy, SudoRPCCallProxyCallback, SudoRPCReturn, SudoRPCService } from "@sudorpc/core";
+import { SudoRPCCall, SudoRPCCallProxy, SudoRPCCallProxyCallback, SudoRPCReturn, SudoRPCServiceErrorGenerator } from "@sudorpc/core";
+import Axios, { AxiosError, AxiosResponse } from "axios";
 
 export class SudoRPCAxiosProxy<Metadata, Payload, SuccessResult, FailResult>
     extends SudoRPCCallProxy<Metadata, Payload, SuccessResult, FailResult> {
 
     public static create<Metadata, Payload, SuccessResult, FailResult>(
-        service: SudoRPCService<Metadata, Payload, SuccessResult, FailResult>,
+        endpoint: string,
     ): SudoRPCAxiosProxy<Metadata, Payload, SuccessResult, FailResult> {
 
-        return new SudoRPCAxiosProxy(service);
+        return new SudoRPCAxiosProxy(
+            endpoint,
+        );
     }
 
-    private readonly _service:
-        SudoRPCService<Metadata, Payload, SuccessResult, FailResult>;
+    private readonly _endpoint: string;
 
     private readonly _listeners:
         Map<string, SudoRPCCallProxyCallback<SuccessResult, FailResult>>;
 
     private constructor(
-        service: SudoRPCService<Metadata, Payload, SuccessResult, FailResult>,
+        endpoint: string,
     ) {
 
         super();
 
-        this._service = service;
+        this._endpoint = endpoint;
 
         this._listeners = new Map();
     }
@@ -37,16 +39,33 @@ export class SudoRPCAxiosProxy<Metadata, Payload, SuccessResult, FailResult>
         call: SudoRPCCall<Metadata, Payload>,
     ): void {
 
-        this._service.execute(call).then(
-            (
-                message: SudoRPCReturn<SuccessResult, FailResult>,
-            ) => {
+        Axios.post(this._endpoint, call)
+            .then(
+                (
+                    response: AxiosResponse<SudoRPCReturn<SuccessResult, FailResult>>,
+                ) => {
+
+                    for (const listener of this._listeners.values()) {
+                        listener(response.data);
+                    }
+                },
+            )
+            .catch((error: AxiosError) => {
+
+                const errorGenerator: SudoRPCServiceErrorGenerator<Metadata, Payload, SuccessResult, FailResult>
+                    = SudoRPCServiceErrorGenerator.create(call);
+
+                const errorResult: SudoRPCReturn<SuccessResult, FailResult>
+                    = errorGenerator.createErrors([{
+                        error: error.name,
+                        message: error.message,
+                        result: error.toJSON() as any,
+                    }]);
 
                 for (const listener of this._listeners.values()) {
-                    listener(message);
+                    listener(errorResult);
                 }
-            },
-        );
+            });
     }
 
     public addListener(
